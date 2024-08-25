0cc44baa73e8668a302fa6629c652260364b60e7
paschoalAr <arthur.paschoal@edu.pucrs.br>
2024-08-25 10:49:29 -0300

    Implementação do carro de tipo SUV

--- src/App.java
+++ src/App.java
@@ -43,5 +43,20 @@ public class App {
         utilitario.viaja(250);
         utilitario.viaja(150);
         System.out.println(utilitario);
+
+        Carro suv = new Carro("SUV", TipoCombustivel.FLEX, 8, 55);
+
+        System.out.println("\nTipos de veiculos:");
+        System.out.println(suv);
+
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro basico com gasolina");
+        suv.abastece(TipoCombustivel.GASOLINA, 55);
+        System.out.println(suv);
+        System.out.println("\nViajando com o carro basico");
+        suv.viaja(250);
+        suv.viaja(150);
+        System.out.println(suv);
+
     }
 }
