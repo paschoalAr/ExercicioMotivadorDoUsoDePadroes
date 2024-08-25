e49c1e8973f41420ff4adcdbf6d979d3e43ed9e7
paschoalAr <arthur.paschoal@edu.pucrs.br>
2024-08-25 10:46:16 -0300

    Implementação do carro de tipo utilitario

--- src/App.java
+++ src/App.java
@@ -29,5 +29,19 @@ public class App {
         esportivo.viaja(250);
         esportivo.viaja(150);
         System.out.println(esportivo);
+
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
+
+        System.out.println("\nTipos de veiculos:");
+        System.out.println(utilitario);
+
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro basico com gasolina");
+        utilitario.abastece(TipoCombustivel.DIESEL, 70);
+        System.out.println(utilitario);
+        System.out.println("\nViajando com o carro basico");
+        utilitario.viaja(250);
+        utilitario.viaja(150);
+        System.out.println(utilitario);
     }
 }
